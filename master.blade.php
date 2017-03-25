2a7ef5109c6c94cb4d5d7aacc3b3f383869e474d
satriapribadi03 <satria.synder@gmail.com>
2017-03-25 12:25:27 +0800

    Tugas Posttest 4 Selesai

--- master.blade.php
+++ master.blade.php
@@ -80,7 +80,7 @@
         <nav class="navbar navbar-default navbar-fixed-bottom">
         <footer class="container">
             <!-- Please dont delete this -->
-            created by <a href="https://www.facebook.com/asyharul.maarif"><span><i class="fa fa-facebook" style="color:#1da1f2"></i> @SatriaPribadi</span></a>
+            created by <a href="https://www.facebook.com/satria.peacer"><span><i class="fa fa-facebook" style="color:#1da1f2"></i> @SatriaPribadi</span></a>
             <!--/ Please dont delete this -->
         </footer>   
         </nav>
